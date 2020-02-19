2f8c79557a5925e4d7850449d395c35451937133
Big_HM <Big_HM@126.com>
2020-02-19 14:35:53 +0800

    init nuxtjs+argon+crimso

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div contain-fluid>
+    <app-header></app-header>
+    <nuxt />
+    <app-footer></app-footer>
+  </div>
+</template>
+<script>
+import appHeader from '@/components/partials/Header1'
+import appFooter from '@/components/partials/Footer1'
+
+export default {
+  components: {
+    appHeader,
+    appFooter
+  }
+}
+</script>
+
+<style></style>
