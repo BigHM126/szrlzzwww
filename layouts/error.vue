19a725d7487632a505f7faa643f3eb9306ca2ee3
Big_HM <Big_HM@126.com>
2020-02-20 16:54:18 +0800

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <section class="error-area">
+      <div class="d-table">
+        <div class="d-table-cell">
+          <div class="container">
+            <div class="error-content">
+              <img src="~assets/img/404.png" alt="error" />
+
+              <h3>页面走丢了</h3>
+              <p>
+                页面链接已失效或已取消！换个姿势打开试试！
+              </p>
+
+              <a href="/" class="default-btn-one">
+                返回首页
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
